bfa454cf15503b65ee597738b578ec06754f7d74
mistermboy <pabloyo97@hotmail.com>
2019-10-25 09:32:58 +0200

    ShapeComponent

--- src/components/Assistant.js
+++ src/components/Assistant.js
@@ -1,6 +1,7 @@
 
 import React,{Component} from 'react';
 import '../App.css';
+import ShapeComponent from './shexComponents/ShapeComponent';
 
 let Shape = require('../entities/shexEntities/shape.js');
 let Triple = require('../entities/shexEntities/triple.js');
@@ -26,57 +27,12 @@ class Assistant extends Component {
 
     }
 
-    getType(element){
-        return { __html: element.type.getHtml()}
-    }
-
     render(){
         return <div id='assistant-container' className="assistant col-lg-6" > 
                 <button id='addShapeButton' className="btn-primary addShapeButton">+ Shape</button>
                 {this.state.shapes.map(shape =>
                 
-                    <div key={shape.id} className="shapes-container">
-                        <div className="row shapes-header">
-                            <label className="col-sm-2">Shape </label>
-                            <select className="col-sm-2 form-control shapeType">
-                                <option value="iriRef">IriRef</option>
-                                <option value="prefixedIri">Prefixed</option>
-                                <option value="bnode">BNode</option>
-                            </select>
-
-                            <div className="row col-sm-2" dangerouslySetInnerHTML={this.getType(shape)} />
-
-                            <button className="col-xs-10 deleteShapeButton mdc-icon-button material-icons btn-danger">delete</button>
-                        </div>
-                        <button className="btn-primary addPropButton col-xs-3">+ Triple</button>
-                        
-                        <div className="triples-container col-xs">
-                            {shape.triples.map(triple =>
-                            
-                                <div key={triple.id} className="row tripleRow">
-                                    <div className="row triples-header">
-                                        <label className="col-sm-1 tripleLabel">Triple</label>                        
-                                        <select className="col-sm-2 form-control tripleType">
-                                            <option value="iriRef">IriRef</option>
-                                            <option value="prefixedIri">Prefixed</option>
-                                        </select>
-                            
-                                        <div className="row col-sm-4" dangerouslySetInnerHTML={this.getType(triple)} /> 
-                                    
-                                        <select className="col-sm-3  form-control tripleCardinality">
-                                            <option value="">Exactly one</option>
-                                            <option value="*">Zero or more</option>
-                                            <option value="+">One at least</option>
-                                            <option value="?">One or none</option>
-                                        </select>
-
-                                        <button className="col-xs-10 deletePropButton mdc-icon-button material-icons btn-danger">delete</button>
-
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-                    </div>
+                    <ShapeComponent key={shape.id} shape={shape} />
 
                 )}
             </div>
