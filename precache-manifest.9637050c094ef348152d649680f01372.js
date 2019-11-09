56b397cd875e1c2f49ceda9169e9cb34e6de0978
mistermboy <pabloyo97@hotmail.com>
2019-11-09 14:55:38 +0100

    Updates

---
+++ precache-manifest.9637050c094ef348152d649680f01372.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375c8825346e081e38460bd56dedbc26",
+    "url": "/shex-author/index.html"
+  },
+  {
+    "revision": "7cc7aee17d48da5340fe",
+    "url": "/shex-author/static/css/2.d722c9db.chunk.css"
+  },
+  {
+    "revision": "f719b6b4e2ddc2d7756a",
+    "url": "/shex-author/static/css/main.db65ce8c.chunk.css"
+  },
+  {
+    "revision": "7cc7aee17d48da5340fe",
+    "url": "/shex-author/static/js/2.5d4a55ed.chunk.js"
+  },
+  {
+    "revision": "f719b6b4e2ddc2d7756a",
+    "url": "/shex-author/static/js/main.5eff87d2.chunk.js"
+  },
+  {
+    "revision": "2c7093afc688868c62e3",
+    "url": "/shex-author/static/js/runtime-main.95ebbff4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/shex-author/static/media/logo.25bf045c.svg"
+  }
+]);
