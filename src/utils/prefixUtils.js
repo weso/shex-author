eceac88343d926f16029ebf1e413b18ab5496d09
mistermboy <pabloyo97@hotmai.com>
2020-02-25 11:45:38 +0000

    PrefixSelector refactor

--- src/utils/prefixUtils.js
+++ src/utils/prefixUtils.js
@@ -1,3 +1,4 @@
+import Codemirror from 'codemirror';
 import Editor from '../entities/editor';
 import Prefix from '../entities/shexEntities/shexUtils/prefix.js';
 
@@ -11,3 +12,18 @@ export function getPrefix(prefix){
       return new Prefix();
 }
 
+export function addPrefix(prefix,namespaces){
+        let yashe = Editor.getInstance().getYashe();
+        let current = yashe.getValue();
+        let defined = yashe.getDefinedPrefixes();
+        let uri = 'http://example.org/';
+        //getUri
+        for(let def in namespaces){
+          for(let p in namespaces[def]){
+            if(p==prefix)
+              uri = namespaces[def][p];
+          }
+        }
+        yashe.setValue( 'PREFIX ' + prefix + ': <' + uri + '>\n' + current );
+        Codemirror.signal(yashe,'prefixUpdate');
+  }
