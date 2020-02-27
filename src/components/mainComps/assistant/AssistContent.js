89ba41d0966eede412dda86a01cebf67e9848a4e
mistermboy <pabloyo97@hotmai.com>
2020-02-27 11:44:35 +0000

    Fixed addShapBtn

--- src/components/mainComps/assistant/AssistContent.js
+++ src/components/mainComps/assistant/AssistContent.js
@@ -1,17 +1,19 @@
 import React,{useContext} from 'react';
 import {AppContext} from '../../../App';
+import {AssistContext} from '../Assistant';
 import ShapeComponent from '../shEx/ShapeComponent';
 
 function AssistContent (props) {
 
     const context = useContext(AppContext);
+    const assistContext =  useContext(AssistContext);
 
     return ( 
-    <div className={context.asist}>
+    <div className={assistContext.asist}>
         <div id='assistant-container' className='assistantContainer'> 
             {context.shapes.map(shape =>{return  <ShapeComponent shape={shape} key={shape.id}/> })}
                 <div className="addCont">
-                    <button className={context.addBtns+" addShapeButton"} 
+                    <button className={assistContext.addBtns+" addShapeButton"} 
                             onClick={context.addShape}
                             title="Add Shape">
                             + Shape
