f6174e9db4c00bf74cae0a7376a379284d087051
mistermboy <pabloyo97@hotmai.com>
2020-03-16 12:10:56 +0000

    AddPrefixes

--- src/components/prefixAssist/PrefixComp.js
+++ src/components/prefixAssist/PrefixComp.js
@@ -1,41 +1,66 @@
-import React,{useContext,useState} from 'react';
+import React,{useContext,useState,useEffect} from 'react';
 import {AppContext} from '../../App';
 import { Collapse } from 'reactstrap';
 import { Resizable } from "re-resizable";
 import AssistTitle from '../mainComps/assistant/AssistTitle';
+import Editor from '../../entities/editor';
+import Prefix from '../../entities/shexEntities/shexUtils/prefix';
+
 import '../../css/shexComponents/headers/PrefixHeader.css';
 
 function PrefixComp (props) {
 
         const context = useContext(AppContext);
+        
         let open = '<';
         let close = '>';
+        const defaultPrefixes = [
+                new Prefix('','http://example.org/'),
+                new Prefix('schema','http://schema.org/'),
+                new Prefix('xsd','http://www.w3.org/2001/XMLSchema#')
+        ]
+        const [prefixes,setPrefixes]=useState(defaultPrefixes);
+
+        const addPrefix = function(){
+                setPrefixes([...prefixes,new Prefix('','')]);
+        }
+      
+        useEffect(() => {
+        // Actualiza el título del documento usando la API del navegador
+       // prefixes.push()
+                
+        });
+      
         return (
                 <div>
-                    <div className='prefixHeader'>            
-                        <input  type="text" 
-                                className="name"
-                                placeholder="eg: schema"
-                                title="Alias"/>
-                        <label  className={context.shapeLabel+" shapeNameLabel"}>:</label>
-                        <label  className={context.shapeLabel+" shapeNameLabel"}>{open}</label>
-                        <input  type="text" 
-                                className="name"
-                                placeholder="eg: http://schema.org/"
-                                title="IRI"/>
-                        <label  className={context.shapeLabel+" shapeNameLabel"}>{close}</label>
-                        <button className="deletePrefix mdc-icon-button material-icons" 
-                                title="Delete Prefix">
-                                delete
+                {prefixes.map(prefix =>{
+                        return(<div  key={prefix.prefixName} className='prefixHeader'>            
+                                        <input  type="text" 
+                                                className="name"
+                                                placeholder="eg: schema"
+                                                value={prefix.prefixName}
+                                                title="Alias"/>
+                                        <label  className={context.shapeLabel+" prefixLabel"}>:</label>
+                                        <label  className={context.shapeLabel+" prefixLabel"}>{open}</label>
+                                        <input  type="text" 
+                                                className="name"
+                                                value={prefix.prefixValue}
+                                                placeholder="eg: http://schema.org/"
+                                                title="IRI"/>
+                                        <label  className={context.shapeLabel+" prefixLabel"}>{close}</label>
+                                        <button className="deletePrefix mdc-icon-button material-icons" 
+                                                title="Delete Prefix">
+                                                delete
+                                        </button>                              
+                                </div>)
+                })}
+                        <div className="addCont">
+                        <button className={context.addBtns+" addPrefix"} 
+                                onClick={addPrefix}
+                                title="Add Prefix">
+                                + Prefix
                         </button>
-                    </div>
-                     <div className="addCont">
-                    <button className={context.addBtns+" addShapeButton"} 
-                            onClick={context.addShape}
-                            title="Add Shape">
-                            + Prefix
-                    </button>
-                </div>
+                        </div>   
                 </div>
 );
 }
