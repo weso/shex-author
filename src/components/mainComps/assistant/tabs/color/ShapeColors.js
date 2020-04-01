d6f13a5c3028ae4598e769b5dfb8506d77056911
mistermboy <pabloyo97@hotmai.com>
2020-04-01 10:34:07 +0100

    pickers in properties

--- src/components/mainComps/assistant/tabs/color/ShapeColors.js
+++ src/components/mainComps/assistant/tabs/color/ShapeColors.js
@@ -3,132 +3,18 @@ import {AssistContext} from '../../../Assistant';
 import ShapeView from './views/ShapeView';
 import TripleView from './views/TripleView';
 import ColorComp from './utils/ColorComp';
+import {SHAPE_PICKERS,TRIPLE_PICKERS} from '../../../../../conf/properties';
 
 function ShapeColors (props) {
 
         const assistContext = useContext(AssistContext);
-        const shapePickers = [
-        {
-            tag:'Labels',
-            element:'label'
-        },
-        {
-            tag:'Header',
-            element:'header'
-        },
-        {
-            tag:'Custom',
-            element:'custom'
-        },
-        {
-            tag:'Custom Fill',
-            element:'customFill'
-        },
-         {
-            tag:'Delete',
-            element:'delete'
-        },
-        {
-            tag:'Delete Fill',
-            element:'deleteFill'
-        },
-        {
-            tag:'Collapse',
-            element:'collapse'
-        },
-       
-        {
-            tag:'Shape Body',
-            element:'body'
-        },
-        {
-            tag:'+Triple',
-            element:'addTriple'
-        },
-        {
-            tag:'+Triple Fill',
-            element:'addTripleFill'
-        }
         
-        
-        ];
-
-        const asd = [
-        {
-            tag:'Header',
-            element:'custom'
-        },
-        {
-            tag:'Custom',
-            element:'custom'
-        },
-        {
-            tag:'Custom Fill',
-            element:'customFill'
-        },
-        {
-            tag:'Constraint',
-            element:'custom'
-        },
-        {
-            tag:'Constraint Fill',
-            element:'customFill'
-        },
-
-        {
-            tag:'Facet',
-            element:'custom'
-        },
-        {
-            tag:'Facet Fill',
-            element:'customFill'
-        },
-
-        {
-            tag:'ShapeRef',
-            element:'custom'
-        },
-        {
-            tag:'ShapeRef Fill',
-            element:'customFill'
-        },
-
-        {
-            tag:'Cardinality',
-            element:'custom'
-        },
-        {
-            tag:'Cardinality Fill',
-            element:'customFill'
-        },
-         {
-            tag:'Delete',
-            element:'delete'
-        },
-        {
-            tag:'Delete Fill',
-            element:'deleteFill'
-        },
-        {
-            tag:'Collapse',
-            element:'collapse'
-        },
-       
-        {
-            tag:'Triple Body',
-            element:'body'
-        },
-       
-        
-        
-        ];
-
         return ( <div className="shape" style={assistContext.styles.header}>
                     <ShapeView/>
                     <TripleView/>
-                    <ColorComp customClass='customShape' pickers={shapePickers}/>
+                    <ColorComp customClass='customShape' pickers={SHAPE_PICKERS}/>
                     
-                    <ColorComp customClass='customTriple' pickers={asd}/>
+                    <ColorComp customClass='customTriple' pickers={TRIPLE_PICKERS}/>
                     <div className="triples" style={assistContext.styles.body}>
                         <button className="addTripleButton"
                                 style={assistContext.styles.triple} 
--- src/conf/properties.js
+++ src/conf/properties.js
@@ -24,3 +24,112 @@ export const TRIPLE_COLORS = {
         addTripleFill:'#EFDECD',
 }
 
+
+export const SHAPE_PICKERS = [
+        {
+            tag:'Labels',
+            element:'label'
+        },
+        {
+            tag:'Header',
+            element:'header'
+        },
+        {
+            tag:'Custom',
+            element:'custom'
+        },
+        {
+            tag:'Custom Fill',
+            element:'customFill'
+        },
+         {
+            tag:'Delete',
+            element:'delete'
+        },
+        {
+            tag:'Delete Fill',
+            element:'deleteFill'
+        },
+        {
+            tag:'Collapse',
+            element:'collapse'
+        },
+       
+        {
+            tag:'Shape Body',
+            element:'body'
+        },
+        {
+            tag:'+Triple',
+            element:'addTriple'
+        },
+        {
+            tag:'+Triple Fill',
+            element:'addTripleFill'
+        }];
+
+export const TRIPLE_PICKERS = [
+        {
+            tag:'Header',
+            element:'header'
+        },
+        {
+            tag:'Custom',
+            element:'custom'
+        },
+        {
+            tag:'Custom Fill',
+            element:'customFill'
+        },
+        {
+            tag:'Constraint',
+            element:'constraint'
+        },
+        {
+            tag:'Constraint Fill',
+            element:'constraintFill'
+        },
+
+        {
+            tag:'Facet',
+            element:'facet'
+        },
+        {
+            tag:'Facet Fill',
+            element:'facetFill'
+        },
+
+        {
+            tag:'ShapeRef',
+            element:'shapeRef'
+        },
+        {
+            tag:'ShapeRef Fill',
+            element:'shapeRefFill'
+        },
+
+        {
+            tag:'Cardinality',
+            element:'cardinality'
+        },
+        {
+            tag:'Cardinality Fill',
+            element:'cardinalityFill'
+        },
+         {
+            tag:'Delete',
+            element:'delete'
+        },
+        {
+            tag:'Delete Fill',
+            element:'deleteFill'
+        },
+        {
+            tag:'Collapse',
+            element:'collapse'
+        },
+       
+        {
+            tag:'Triple Body',
+            element:'body'
+        }];
