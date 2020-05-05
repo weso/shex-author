2b77e0ae153b1a3fafbb84557557d6dc7f0c2f85
mistermboy <pabloyo97@hotmail.com>
2020-05-05 16:29:21 +0100

    yeah, i think it's fixed

--- src/galery/defaultExample.js
+++ src/galery/defaultExample.js
@@ -1,17 +1,17 @@
-/* export const defaultExample = 
+export const defaultExample = 
 
 'PREFIX :       <http://example.org/>\n'+
 'PREFIX schema: <http://schema.org/>\n'+
 'PREFIX xsd:    <http://www.w3.org/2001/XMLSchema#>\n\n'+
 
 ':User IRI {\n'+
-'  schema:name          xsd:string @:User ;\n'+
+'  schema:name          xsd:string   ;\n'+
 '  schema:birthDate     xsd:date   ? ;\n'+
 '  schema:birthPlace    xsd:string + ;\n'+
 '  schema:knows         @:User     *  \n'+
-'}\n'; */
+'}\n';
 
-export const defaultExample = 
+/* export const defaultExample = 
 `
 PREFIX :    <http://example.org/>
 PREFIX schema:    <http://schema.org/>
@@ -20,7 +20,7 @@ PREFIX xsd:    <http://www.w3.org/2001/XMLSchema#>
 :User {
    schema:0 xsd:string {schema:1 . ; schema:2 .} {2,3}
 }`
-
+ */
 /* export const defaultExample = 
 
 'PREFIX :       <http://example.org/>\n'+
@@ -31,3 +31,11 @@ PREFIX xsd:    <http://www.w3.org/2001/XMLSchema#>
 '  schema:0 xsd:string {schema:1 . ; schema:2 .} +\n '+
 '}\n';
  */
+
+
+/*  :human {
+  schema:0 . + ;
+  schema:1 {schema:11 .} + ;
+  schema:2 {schema:21 . +} + ;
+  schema:3 {schema:31 {schema:311 .+} +} + ;
+} */
