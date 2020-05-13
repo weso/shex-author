794852b3ecdc4d93fa6edc1196266d22315eedfb
mistermboy <pabloyo97@hotmail.com>
2020-05-13 22:12:02 +0100

    minor changes

--- src/galery/defaultExample.js
+++ src/galery/defaultExample.js
@@ -11,7 +11,3 @@ export const defaultExample =
 '  schema:knows         @:User     *  \n'+
 '}\n';
 
-/*
-
-`PREFIX p: <http://www.wikidata.org/prop/> PREFIX ps: <http://www.wikidata.org/prop/statement/> PREFIX pq: <http://www.wikidata.org/prop/qualifier/> PREFIX wd: <http://www.wikidata.org/entity/> PREFIX wdt: <http://www.wikidata.org/prop/direct/> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> <#operating_system> { p:P275 { ps:P275 xsd:string + ; ps:P275 IRI + ; ps:P275 xsd:stringdsfsd + ; ps:P275 IRI + ; ps:P275 IRI + ; } ; p:P178 { ps:P178 IRI ; ps:P275 IRI + ; ps:P275 IRI + ; }? ; p:P176 { ps:P176 IRI ; ps:P275 IRI + ; ps:P275 IRI + ; }? ; p:P910 { ps:P910 IRI ; ps:P275 IRI + ; ps:P275 IRI + ; }? ; p:P373 { ps:P373 xsd:string * ; ps:P275 IRI + ; ps:P275 IRI + ; }? ; }
-` */
